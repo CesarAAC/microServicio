f933580e0f8a35427861393e0159607d77846445
JorgeB <j.bustamantep@uniandes.edu.co>
2024-05-30 06:29:40 -0500

    cambios

--- BasedeDatos/src/main/java/com/MicroServicio/BasedeDatos/controlador/ClienteController.java
+++ BasedeDatos/src/main/java/com/MicroServicio/BasedeDatos/controlador/ClienteController.java
@@ -8,8 +8,7 @@ import com.MicroServicio.BasedeDatos.repositorio.ClienteRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
